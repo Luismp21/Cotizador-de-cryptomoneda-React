997a9d7f2cebffe43949183c44682dc2bcf67bda
Luis <luis.m.pinilla21@gmail.com>
2023-02-05 12:52:20 -0300

    Primer commit

---
+++ src/component/Resultado.jsx
@@ -0,0 +1,61 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+
+const Imagen = styled.img`
+  display: block;
+  width: 120px;
+`;
+
+const Texto = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Precio = styled.p`
+  font-size: 24px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+export const Resultado = ({ resultado }) => {
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
+    resultado;
+
+  return (
+    <Container>
+      <Imagen
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="Imagen-crypt"
+      />
+      <div>
+        <Precio>
+          El Precio es: <span>{PRICE}</span>
+        </Precio>
+        <Texto>
+          El Precio más alto del día: <span>{HIGHDAY}</span>{" "}
+        </Texto>
+        <Texto>
+          El precio más bajo del día: <span>{LOWDAY}</span>{" "}
+        </Texto>
+        <Texto>
+          Variación ultimas 24 horas: <span>{CHANGEPCT24HOUR}</span>{" "}
+        </Texto>
+        <Texto>
+          Última Actualización: <span>{LASTUPDATE}</span>
+        </Texto>
+      </div>
+    </Container>
+  );
+};
---
+++ src/data/monedas.js
@@ -0,0 +1,8 @@
+export const monedas = [
+  { id: "CLP", nombre: "Peso Chileno" },
+  { id: "USD", nombre: "Dolar de Estados Unidos" },
+  { id: "MXN", nombre: "Peso Mexicano" },
+  { id: "EUR", nombre: "Euro" },
+  { id: "GBP", nombre: "Libra Esterlina" },
+  { id: "ARS", nombre: "Peso Argentino" },
+];
